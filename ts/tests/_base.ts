34474b58c586762bbf11f677e602f20a846be8d3
fmasclef <fmasclef@pm.me>
2021-09-04 14:17:58 +0200

    test: this should fail testing

--- ts/tests/_base.ts
+++ ts/tests/_base.ts
@@ -13,7 +13,7 @@ describe('Basic server checks', () => {
         chai.expect(res).to.have.status(200);
         chai.expect(res.header["content-type"]).to.contains('application/json');
         chai.expect(res.body).to.be.an('object');
-        chai.expect(res.body).to.have.property('greetings').eql('Hello!');
+        chai.expect(res.body).to.have.property('greetings').eql('hello!');
         done();
       });
   });
